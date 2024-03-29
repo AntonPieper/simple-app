51c2bcdd31bb24ca3d0125003d4b72f778eb6a7e
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2024-03-29 17:09:43 +0100

    Add backend /metrics prometheus endpoint

--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import FastifyHelmet from "@fastify/helmet";
+import FastifyMetrics from "fastify-metrics";
 import postgres from "postgres";
 import {
   Type,
@@ -60,75 +61,86 @@ fastify.addHook("onClose", async () => {
   return sql.end();
 });
 
-const IdParamSchema = Type.Object({ id: Type.Number() });
-
-// -- Routes --
-
-type Client = {
-  index: number;
-  stream: ServerResponse<IncomingMessage>;
-};
-
-const clients: Client[][] = [];
-fastify.get(
-  "/:id",
-  { schema: { params: IdParamSchema } },
-  async (req, reply) => {
-    fastify.log.trace("Got request");
-    const answer =
-      await sql`SELECT count FROM clicks WHERE id = ${req.params.id}`;
-    fastify.log.trace("Selected clicks");
-    const counterID = req.params.id;
-    if (answer.length === 0) {
-      fastify.log.trace("No clicks");
-      reply.status(404);
-      return `ID ${counterID} not found`;
-    }
-    if (counterID >= clients.length) {
-      clients.length = counterID + 1;
-    }
-    clients[counterID] ??= [];
-    const client: Client = {
-      index: clients[counterID].length,
-      stream: reply.raw,
-    };
-    clients[counterID].push(client);
-    fastify.log.trace(`Writing head for ${client.index}`);
-    client.stream.writeHead(200, {
-      "Content-Type": "text/event-stream",
-      Connection: "keep-alive",
-      "Cache-Control": "no-cache",
-    });
-    req.raw.on("close", () => {
-      fastify.log.debug(`Closing ${client.index}`);
-      const listeners = clients[counterID];
-      const last = listeners[listeners.length - 1];
-      last.index = client.index;
-      listeners[client.index] = last;
-    });
-    fastify.log.debug(`[Initial] Sending ${answer[0].count}...`);
-    client.stream.write(`data: ${answer[0].count}\n\n`);
-  }
-);
-
-fastify.post("/:id/add", { schema: { params: IdParamSchema } }, async (req) => {
-  const id = req.params.id;
-  const result =
-    await sql`UPDATE clicks SET count = count + 1 WHERE id = ${id} RETURNING count`;
-  if (clients[id] !== undefined) {
-    for (const row of result) {
-      for (const client of clients[id]) {
-        fastify.log.debug(`[Broadcast] Sending ${row.count}...`);
-        client.stream.write(`data: ${row.count}`);
-      }
-    }
-  }
-  return result.length > 0;
-});
-
-fastify.listen({ port, host }, (err) => {
+fastify.register(FastifyMetrics, { endpoint: "/metrics" }).after((err) => {
   if (err !== null) {
-    fastify.log.error(err);
-    process.exit(1);
+    fastify.log.fatal(err);
+    return;
   }
+
+  const IdParamSchema = Type.Object({ id: Type.Number() });
+
+  // -- Routes --
+
+  type Client = {
+    index: number;
+    stream: ServerResponse<IncomingMessage>;
+  };
+
+  const clients: Client[][] = [];
+  fastify.get(
+    "/:id",
+    { schema: { params: IdParamSchema } },
+    async (req, reply) => {
+      fastify.log.trace("Got request");
+      const answer =
+        await sql`SELECT count FROM clicks WHERE id = ${req.params.id}`;
+      fastify.log.trace("Selected clicks");
+      const counterID = req.params.id;
+      if (answer.length === 0) {
+        fastify.log.trace("No clicks");
+        reply.status(404);
+        return `ID ${counterID} not found`;
+      }
+      if (counterID >= clients.length) {
+        clients.length = counterID + 1;
+      }
+      clients[counterID] ??= [];
+      const client: Client = {
+        index: clients[counterID].length,
+        stream: reply.raw,
+      };
+      clients[counterID].push(client);
+      fastify.log.trace(`Writing head for ${client.index}`);
+      client.stream.writeHead(200, {
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive",
+        "Cache-Control": "no-cache",
+      });
+      req.raw.on("close", () => {
+        fastify.log.debug(`Closing ${client.index}`);
+        const listeners = clients[counterID];
+        const last = listeners[listeners.length - 1];
+        last.index = client.index;
+        listeners[client.index] = last;
+      });
+      fastify.log.debug(`[Initial] Sending ${answer[0].count}...`);
+      client.stream.write(`data: ${answer[0].count}\n\n`);
+    },
+  );
+
+  fastify.post(
+    "/:id/add",
+    { schema: { params: IdParamSchema } },
+    async (req) => {
+      const id = req.params.id;
+      const result =
+        await sql`UPDATE clicks SET count = count + 1 WHERE id = ${id} RETURNING count`;
+      if (clients[id] !== undefined) {
+        for (const row of result) {
+          for (const client of clients[id]) {
+            fastify.log.debug(`[Broadcast] Sending ${row.count}...`);
+            client.stream.write(`data: ${row.count}`);
+          }
+        }
+      }
+      return result.length > 0;
+    },
+  );
+
+  fastify.listen({ port, host }, (err) => {
+    if (err !== null) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+  });
 });
