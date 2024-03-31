6e17356219d28c60ea05861ffc0bf3f5bf7e5aca
AntonPieper <anton.pieper@informatik.hs-fulda.de>
2024-03-31 12:14:39 +0200

    Add automatic db table generation

--- setup-db.sql
+++ setup-db.sql
@@ -1,4 +1,17 @@
-CREATE TABLE clicks (id SERIAL PRIMARY KEY, count INT);
-INSERT INTO clicks (count)
-SELECT 0
-FROM generate_series(1, 10);
+-- Create the table only if it doesn't exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'clicks') THEN
+        CREATE TABLE public.clicks (id SERIAL PRIMARY KEY, count INT);
+    END IF;
+END $$;
+
+-- Insert data only if the table is empty
+DO $$
+BEGIN
+    IF (SELECT COUNT(*) FROM public.clicks) = 0 THEN
+        INSERT INTO public.clicks (count)
+        SELECT 0
+        FROM generate_series(1, 10);
+    END IF;
+END $$;
